8b6c315a684b68477d8410c30861ffb255506061
Syed Fasih <elite9145build@gmail.com>
2025-04-24 20:23:49 +0500

    GET Users API done

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -26,6 +28,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
